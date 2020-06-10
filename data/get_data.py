0fc6c5641fdbc11c0ea766bc314f3ea90ebeaa64
Michael Patel <michael.patel94@gmail.com>
2020-06-10 14:32:02 -0400

    Got all image links

--- data/get_data.py
+++ data/get_data.py
@@ -12,9 +12,14 @@ File description:
 # Import
 import os
 import re
+from time import sleep
 import urllib.request
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 ################################################################################
@@ -22,11 +27,58 @@ from selenium import webdriver
 if __name__ == "__main__":
     # use selenium to load dynamic data
     # webdriver.Chrome(path to chromedriver)
+    chromedriver_path = os.path.join(os.getcwd(), "chromedriver.exe")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(
+        chromedriver_path,
+        options=chrome_options
+    )
     # .get(url)
+    url = "https://thefootballcrestindex.com/blogs/premier-league-clubs"
+    driver.get(url)
+
+    # click "View More" buttons
+    images = []
+    for i in range(10):
+        x = driver.find_elements_by_tag_name("img")
+        for j in x:
+            s = j.get_attribute("src")
+            if "articles" in s:
+                if s not in images:
+                    images.append(s)
+
+        buttons = driver.find_elements_by_xpath("//*[contains(text(), 'View More')]")
+        for b in buttons:
+            b.click()
+
+        #sleep(1)
+
+    print(f'Number of images: {len(images)}')
+    for i in images:
+        print(i)
+    quit()
+
+    """
+    z = driver.find_elements_by_tag_name("img")
+    for i in z:
+        print(i.get_attribute("src"))
+    """
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    driver.close()
+
+    x = soup.findAll("img")
+    for i in x:
+        print(i)
+
+    #print(soup.prettify())
+
+    quit()
 
     # soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    with urllib.request.urlopen("https://thefootballcrestindex.com/blogs/premier-league-clubs") as response:
+    with urllib.request.urlopen() as response:
         page = response.read()
 
     #page = re.sub('<!--|-->', "", str(page))
