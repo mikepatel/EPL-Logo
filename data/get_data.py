25b1bf94e0075ebefb13d6225fbdfb0cb9f274e3
Michael Patel <michael.patel94@gmail.com>
2020-06-11 00:14:21 -0400

    Removing unused imports

--- data/get_data.py
+++ data/get_data.py
@@ -11,16 +11,9 @@ File description:
 ################################################################################
 # Import
 import os
-import re
-from time import sleep
 import requests
-import urllib.request
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 
 ################################################################################
