d6984e066adf1267b2712778cf7ad1d08a0557c3
Michael Patel <michael.patel94@gmail.com>
2020-06-10 23:41:39 -0400

    Gotten all names and image urls

--- data/get_data.py
+++ data/get_data.py
@@ -27,7 +27,6 @@ from selenium.webdriver.common.by import By
 # Main
 if __name__ == "__main__":
     # use selenium to load dynamic data
-    # webdriver.Chrome(path to chromedriver)
     chromedriver_path = os.path.join(os.getcwd(), "chromedriver.exe")
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -35,83 +34,53 @@ if __name__ == "__main__":
         chromedriver_path,
         options=chrome_options
     )
-    # .get(url)
     url = "https://thefootballcrestindex.com/blogs/premier-league-clubs"
     driver.get(url)
 
-    # click "View More" buttons
-    images = []
-    for i in range(10):
-        x = driver.find_elements_by_tag_name("img")
-        for j in x:
-            s = j.get_attribute("src")
-            if "articles" in s:
-                if s not in images:
-                    images.append(s)
+    # parse html
+    names = []
+    image_urls = []
 
+    for i in range(10):  # guessing 10 pages
+        # get club name
+        hrefs = driver.find_elements_by_tag_name("a")
+        for h in hrefs:
+            name = str(h.get_attribute("href"))
+            if "premier-league-clubs/" in name:
+                _, name = name.rsplit("/", 1)
+                if name not in names:
+                    names.append(name)
+
+        # get club image url
+        imgs = driver.find_elements_by_tag_name("img")
+        for img in imgs:
+            s = str(img.get_attribute("src"))
+            if "articles" in s:
+                if s not in image_urls:
+                    image_urls.append(s)
+
+        # load more clubs, "View More" button
         buttons = driver.find_elements_by_xpath("//*[contains(text(), 'View More')]")
         for b in buttons:
             b.click()
 
-        #sleep(1)
+    #
+    print(f'Number of clubs: {len(names)}')
+    print(f'Number of images: {len(image_urls)}')
 
-    # use href text and then parse for club names
-
-    print(f'Number of images: {len(images)}')
-    for i in images:
-        print(i)
-        _, image_file_name = str(i).rsplit("/", 1)
-        image_file_name, _ = str(image_file_name).rsplit("_compact", 1)
-        if "H_" in image_file_name:
-            _, image_file_name = str(image_file_name).rsplit("H_", 1)
-        image_file_name = re.sub(
-            "[0-9]",
-            "",
-            str(image_file_name)
-        )
-        print(image_file_name)
-        print()
-        """
-        with open(image_file_name, "wb") as f:
-            response = requests.get(i, stream=True)
-            if not response.ok:
-                print(response)
-
-            for block in response.iter_content(1024):
-                if not block:
-                    break
-
-                f.write(block)
-        """
-    quit()
-
-    """
-    z = driver.find_elements_by_tag_name("img")
-    for i in z:
-        print(i.get_attribute("src"))
-    """
-
-    soup = BeautifulSoup(driver.page_source, "html.parser")
     driver.close()
-
-    x = soup.findAll("img")
-    for i in x:
-        print(i)
-
-    #print(soup.prettify())
-
     quit()
 
-    # soup = BeautifulSoup(driver.page_source, 'html.parser')
+    """
+    with open(image_file_name, "wb") as f:
+        response = requests.get(i, stream=True)
+        if not response.ok:
+            print(response)
 
-    with urllib.request.urlopen() as response:
-        page = response.read()
+        for block in response.iter_content(1024):
+            if not block:
+                break
 
-    #page = re.sub('<!--|-->', "", str(page))
+            f.write(block)
+    """
 
-    soup = BeautifulSoup(page, "html5lib")
-
-    #image_urls = soup.findAll("div", {"class": "grid--uniform grid--blog article-matrix"})
-    image_urls = soup.findAll("img")
-    for i in image_urls:
-        print(i)
