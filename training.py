fe73f10b2e0e2a8977dff903c1cbd23903957d63
Michael Patel <michael.patel94@gmail.com>
2020-06-11 00:16:45 -0400

    Adding init files

---
+++ training.py
@@ -0,0 +1,12 @@
+"""
+Michael Patel
+June 2020
+
+Project description:
+    CNN for EPL logos
+
+File description:
+    For preprocessing and training
+"""
+################################################################################
+# Import
