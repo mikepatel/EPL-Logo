7ae87a0812b491a3ff807a7b67aecf4ff169fb1e
Michael Patel <michael.patel94@gmail.com>
2020-08-05 00:08:54 -0400

    Adding model defn

--- model.py
+++ model.py
@@ -18,4 +18,55 @@ from parameters import *
 def build_cnn(num_classes):
     model = tf.keras.Sequential()
 
+    # Convolution 1
+    model.add(tf.keras.layers.Conv2D(
+        filters=32,
+        kernel_size=(3, 3),
+        input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS),
+        padding="same",
+        activation=tf.keras.activations.relu
+    ))
+
+    # Convolution 2
+    model.add(tf.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=(3, 3),
+        strides=2,
+        padding="same",
+        activation=tf.keras.activations.relu
+    ))
+
+    # Convolution 3
+    model.add(tf.keras.layers.Conv2D(
+        filters=128,
+        kernel_size=(3, 3),
+        strides=2,
+        padding="same",
+        activation=tf.keras.activations.relu
+    ))
+
+    # Convolution 4
+    model.add(tf.keras.layers.Conv2D(
+        filters=256,
+        kernel_size=(3, 3),
+        strides=2,
+        padding="same",
+        activation=tf.keras.activations.relu
+    ))
+
+    # Flatten
+    model.add(tf.keras.layers.Flatten())
+
+    # Fully Connected
+    model.add(tf.keras.layers.Dense(
+        units=256,
+        activation=tf.keras.activations.relu
+    ))
+
+    # Output
+    model.add(tf.keras.layers.Dense(
+        units=num_classes,
+        activation=tf.keras.activations.softmax
+    ))
+
     return model
